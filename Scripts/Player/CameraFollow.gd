43c3af9b4a11180295da75421514cf8986dc7ca3
ThomasQM <thomas@engineeringvirtue.net>
2018-04-13 18:52:57 -0700

    Added platform and environment (hdrihaven)

--- Scripts/Player/CameraFollow.gd
+++ Scripts/Player/CameraFollow.gd
@@ -1,10 +1,13 @@
 extends Position3D
 
 onready var offsetpos = translation
-var rot_offset = 0 #90
+var rot_offset = 75 setget set_rot_offset, get_rot_offset #90
+
+func set_rot_offset (x): rot_offset = x
+func get_rot_offset(): return rot_offset
 
 func _ready():
 	rotation_degrees.y += rot_offset
 
 func _process(delta):
-	translation = (get_node("../RigidBody").translation)+offsetpos
+	translation = (get_node("../RigidBody").translation)+offsetpos+Vector3(rot_offset/8,0,0)
