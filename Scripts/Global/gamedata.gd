ca5de1c698f57870df352cd2bf936657aa61b2bc
ThomasQM <thomas@engineeringvirtue.net>
2018-04-13 22:50:19 -0700

    Fixed save, added how to play dialog

--- Scripts/Global/gamedata.gd
+++ Scripts/Global/gamedata.gd
@@ -3,14 +3,14 @@ extends Node
 enum Slot {None, TripleJump, Shield}
 const slotarray = ["None", "TripleJump", "Shield"]
 
-var savepath = "user://save.bin"
+var savepath = "user://save.txt"
 var defaultsave = {"level":0, "loadout":[Shield,TripleJump,Shield,TripleJump,Shield,TripleJump]}
 var save = defaultsave setget set_save, get_save
 func set_save(x):
 	save=x
 	var savegame = File.new()
 	
-	savegame.open_encrypted_with_pass(savepath, File.WRITE, OS.get_unique_id())
+	savegame.open(savepath, File.WRITE)
 	savegame.store_var(x)
 	
 func get_save(): return save
@@ -19,9 +19,9 @@ func _ready():
 	var savegame = File.new()
 	
 	if not savegame.file_exists(savepath):
-		savegame.open_encrypted_with_pass(savepath, File.WRITE, OS.get_unique_id())
+		savegame.open(savepath, File.WRITE)
 		savegame.store_var(save)
 	else:
-		savegame.open_encrypted_with_pass(savepath, File.READ, OS.get_unique_id())
+		savegame.open(savepath, File.READ)
 		save = savegame.get_var()
 	savegame.close()
